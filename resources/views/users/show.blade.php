b14e320218aebbd36c1cec909ca43eac23cfc15f
JonasGanderton <jonasganderton@gmail.com>
2023-12-09 18:56:38 +0100

    Display Posts with newest first

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,7 +6,7 @@
 @section('content')
     <h3>Viewing all posts by <i>{{ $userAccount->username }}</i></h3>
     <ul>
-        @foreach ($userAccount->posts as $post)
+        @foreach ($userAccount->posts->sortByDesc('posted_at') as $post)
             @include('posts.linkedPostInline', $post)
             <br>
         @endforeach
