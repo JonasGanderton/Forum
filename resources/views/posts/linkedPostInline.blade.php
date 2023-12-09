bf42dcf96b6fe9170aa6cb1f9dda6d4916c12605
JonasGanderton <jonasganderton@gmail.com>
2023-12-09 19:15:55 +0100

    Add UserAccount and timestamp to inline Post

--- resources/views/posts/linkedPostInline.blade.php
+++ resources/views/posts/linkedPostInline.blade.php
@@ -1,6 +1,15 @@
 <div class="postListItem">
     <a href="{{ route('posts.show', ['post' =>$post]) }}">
         <b>{{ $post->title }}</b>
+        {{--
+            TODO: Truncate content if it's too much for a single 'tile'.
+                  Users then have to open the post to read the rest of it.
+            TODO: Add comment counter.
+        --}}
         {{ $post->content }}
+        <br>
+        <span style="font-size:0.8em">
+            Posted by <i>{{ $post->userAccount->username }}</i> - {{ $post->posted_at }}
+        </span>
     </a>
 </div>
