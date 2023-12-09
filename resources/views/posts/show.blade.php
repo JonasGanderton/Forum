3767138fcaac9e314bce4a3477814e196e91260f
JonasGanderton <jonasganderton@gmail.com>
2023-12-09 18:23:57 +0100

    Add link to UserAccount from Post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,8 +6,11 @@
 @section('content')
     {{-- TODO: Make this into a layout that can be repeated --}}
     <div class="postListItem">
-        <p>Posted by <i>{{ $post->userAccount->username }}</i><br>
-        <i>{{ $post->posted_at}}</i></p>
+        <p>Posted by
+            <i><a href="{{ route('users.show', ['userAccount' => $post->userAccount]) }}">
+                {{ $post->userAccount->username }}
+            </a></i>
+            <br><i>{{ $post->posted_at}}</i></p>
         <p>{{ $post->content }}</p>
         <p>Tags:
             @foreach ($post->tags as $tag)
