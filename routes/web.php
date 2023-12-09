dd40da35614d9454f044c27af0d4f36e1aefa320
JonasGanderton <jonasganderton@gmail.com>
2023-12-09 15:22:09 +0100

    Add routes for post views

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [PostController::class, 'index'])->name('posts.index');
+
+Route::get('/post/{post}', [PostController::class, 'show'])->name('posts.show');
