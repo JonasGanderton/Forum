53fdd0b134730278a31315a290efac58ae843470
JonasGanderton <jonasganderton@gmail.com>
2023-12-11 18:28:52 +0100

    Restyle single UserAccount view

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,9 +1,32 @@
-@extends('app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ $userAccount->username }}
+        </h2>
+    </x-slot>
 
-{{-- TODO: test an invalid title (i.e. attack) --}}
-@section('title', $userAccount->username )
+    <div class="py-12">
+        {{-- TODO:
+            - Find a way of paginating results
+            - Display comments once added too 
+        --}}
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            {{-- TODO: Get posts in a way that enables pagination --}}
+            @include('posts.list', $posts=$userAccount->posts->sortByDesc('posted_at'))
+        </div>
+{{--    <br>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">            
+            <a href="{{ $posts->previousPageUrl() }}" >
+                <x-primary-button>
+                    {{ __('Previous') }}
+                </x-primary-button>
+            </a>
 
-@section('content')
-    <h3>Viewing all posts by <i>{{ $userAccount->username }}</i></h3>
-    @include('posts.list', $posts=$userAccount->posts->sortByDesc('posted_at'))
-@endsection
+            <a href="{{ $posts->nextPageUrl() }}" >
+                <x-primary-button>
+                    {{ __('Next') }}
+                </x-primary-button>
+            </a>
+        </div> --}}
+    </div>
+</x-app-layout>
