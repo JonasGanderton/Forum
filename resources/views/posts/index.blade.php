4b920a5dfa4e57f24598fafc35261e6a355ea305
JonasGanderton <jonasganderton@gmail.com>
2023-12-12 11:45:12 +0100

    Add status message for verification email upon registration

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,6 +6,20 @@
     </x-slot>
 
     <div class="py-12">
+        @if (session('status') === 'verification-link-sent')
+            <div 
+                x-transition:leave="ease-in duration-300"
+                x-transition:leave-start="opacity-100"
+                x-transition:leave-end="opacity-0"
+                style="margin-top:-2.6em" 
+                class="mb-4 text-sm text-gray-600 dark:text-gray-400 max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6"
+                x-data="{ show: true }"
+                x-show="show"
+                x-init="setTimeout(() => show = false, 5000)"
+            >{{ __('A verification link has been emailed to you.') }}
+            </div>
+        @endif
+
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             @include('posts.list', $posts)
         </div>
