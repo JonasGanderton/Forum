6b5036ae08fff329d40342afc546428e0f633a31
JonasGanderton <jonasganderton@gmail.com>
2023-12-13 14:15:34 +0100

    Remove Create Post header bar

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,12 +2,6 @@
 {{-- TODO: Set default styles in a CSS file and reference them (less copy/pasting) --}}
 
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Create Post') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="mx-auto sm:px-6 lg:px-8 space-y-6" style="max-width:60rem">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
